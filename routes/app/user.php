277683aeddd33e7deb1bb8bfac63b259faf31c0f
MrKrab28 <mr.krab2810@gmail.com>
2025-07-28 23:36:20 +0800

    add route prefix for users  routes

---
+++ routes/app/user.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.user.dashboard');
+});
+
+
+Route::group(['prefix' => 'user', 'middleware' => ['auth:user'], 'as' => 'user.'], function () {
+
+
+});
