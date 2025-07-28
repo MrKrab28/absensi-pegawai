5393c1b0e24e79cb549366e0b7fe600bfe47d58a
MrKrab28 <mr.krab2810@gmail.com>
2025-07-28 23:34:49 +0800

    add route prefix for pegawai routes

--- routes/app/pegawai.php
+++ routes/app/pegawai.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.pegawai.dashboard');
 });
+
+
+Route::group(['prefix' => 'pegawai', 'middleware' => ['auth:pegawai'], 'as' => 'pegawai.'], function () {
+    
+
+});
