ccac834e1b6096a255b0cb2740e8215c2dde2d76
MrKrab28 <mr.krab2810@gmail.com>
2025-08-19 20:32:10 +0800

    finishing select tom

--- resources/js/selectTom.js
+++ resources/js/selectTom.js
@@ -1,18 +1,26 @@
 import TomSelect from "tom-select";
+import "tom-select/dist/css/tom-select.css";
 
-// contoh inisialisasi global
-new TomSelect("#pegawai", {
-    create: false,
-    maxItems: 1,
-    sortField: {
-        field: "text",
-        direction: "asc",
-    },
-    onChange: function (value) {
-        if (value) {
-            this.control_input.setAttribute("readonly", "readonly");
-        } else {
-            this.control_input.removeAttribute("readonly");
-        }
-    },
+// otomatis cari semua select yang punya data-tom="true"
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("select[data-tom='true']").forEach((el) => {
+        new TomSelect(el, {
+            create: false,
+            maxItems: el.hasAttribute("multiple") ? null : 1,
+            sortField: {
+                field: "text",
+                direction: "asc",
+            },
+            placeholder: el.getAttribute("placeholder") || "Pilih...",
+            onChange: function (value) {
+                if (!el.hasAttribute("multiple")) {
+                    if (value) {
+                        this.control_input.setAttribute("readonly", "readonly");
+                    } else {
+                        this.control_input.removeAttribute("readonly");
+                    }
+                }
+            },
+        });
+    });
 });
---
+++ resources/views/components/form/select-tom.blade.php
@@ -0,0 +1,24 @@
+@props([
+    'id' => Str::uuid(), // default unik biar tidak bentrok
+    'name',
+    'label' => null,
+    'placeholder' => 'Pilih...',
+    'required' => false,
+    'multiple' => false,
+])
+
+<div class="w-full">
+    @if ($label)
+        <label for="{{ $id }}" class="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">
+            {{ $label }} @if ($required)
+                <span class="text-red-500">*</span>
+            @endif
+        </label>
+    @endif
+
+    <select id="{{ $id }}" name="{{ $name }}{{ $multiple ? '[]' : '' }}"
+        placeholder="{{ $placeholder }}" @if ($required) required @endif
+        @if ($multiple) multiple @endif data-tom="true" autocomplete="off" class="w-full">
+        {{ $slot }}
+    </select>
+</div>
